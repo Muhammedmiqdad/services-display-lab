020022e0e0150fe31bec032eb799036dc99d8008
Miqdad N <124044947+Muhammedmiqdad@users.noreply.github.com>
2025-03-22 12:13:28 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -11,7 +11,7 @@ export default defineConfig(({ mode }) => ({
     port: 8080,
   },
   // Base path - use '/' for root domain or '/your-repo-name/' for GitHub Pages
-  base: mode === 'production' ? 'services-display-lab' : '/',
+  base: mode === 'production' ? '/services-display-lab/' : '/',
   plugins: [
     react(),
     mode === 'development' &&
@@ -23,3 +23,4 @@ export default defineConfig(({ mode }) => ({
     },
   },
 }));
+
